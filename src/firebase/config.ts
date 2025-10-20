82a249096c48679876982995856e2d2dc838e182
بشار الحالمي <basharhafed2020@gmail.com>
2025-10-20 15:09:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2030823377-85b90",
+  "appId": "1:888359470563:web:cdbf73a94bf9cc1cc64cd6",
+  "apiKey": "AIzaSyDtRh7YSi550meDxYLFRiZRZE1ImbXfFRQ",
+  "authDomain": "studio-2030823377-85b90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "888359470563"
+};
