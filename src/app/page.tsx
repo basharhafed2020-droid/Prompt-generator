7d0b5f7620f330b70f7a088d3e15682d47a29b6a
بشار الحالمي <basharhafed2020@gmail.com>
2025-10-25 04:52:44 +0000

    BR (_for element h1_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         <div className="flex items-center gap-4">
             <Wand2 className="h-10 w-10 text-primary" />
             <h1 className="font-headline text-4xl md:text-5xl font-bold tracking-tight">
-                B:) R
+                BR
             </h1>
         </div>
         <div className="flex items-center gap-2">
